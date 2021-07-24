7eb34018d2bacf5341d18cdea9e65b948c3fabbb
SH <tmdgns12j@naver.com>
2021-07-24 14:23:18 +0900

    .

--- test.py
+++ test.py
@@ -1,18 +1,31 @@
-from collections import deque
-def bfs(link,n,infected):
-    queue=deque([n])
-    infected[n]=True
-    while queue:
-        n=queue.popleft()
-        
-    pass
+# from collections import deque
+# a=int(input())#컴퓨터
+# b=int(input())#쌍
+# n=[list(map(int, input().split())) for i in range(b)]#링크 초기화
+# m=[[] for i in range(a+1)]# 노드 재정렬할 배열->컴퓨터+1
+# print('n= ',n)
+# print('m= ',m)
 
+# ch =[0]*(a+1)#감염체크->컴퓨터+1
+# print(ch)
+# for i in range(b): # 노드 재정렬
+#     x,y=n[i]
+#     m[x].append(y)
+#     m[y].append(x)
+# s=1
 
-computer=int(input())
-connect=int(input())
-link=[]
-for i in range(connect):
-    link.append(list(map(int, input().split())))
-infected=[False]*7
-bfs(link,0,infected)
+# def bfs(s):
+#     c=0
+#     dq=deque()
+#     dq.append(s)
+#     ch[s]=1
+#     while len(dq)!=0:
+#         d=dq.popleft()
+#         c+=1
+#         for i in m[d]:
+#             if ch[i]!=1:
+#                 ch[i]=1
+#                 dq.append(i)
+#     print(c-1)
 
+# bfs(s)
