3192a21ae32400277ba9c2c8b326d118ab06a669
SH <tmdgns12j@naver.com>
2021-08-11 19:27:29 +0900

    .

--- 프로그래머스.py
+++ 프로그래머스.py
@@ -49,3 +49,45 @@ def solution(a, b):
     sum=sum%7
     
     return day[sum-1]
+
+#신규 아이디 추천
+import re
+s=".abcdefghijklmn.p"
+s=s.lower()#1
+for i in s:     #2
+    if not i.isalpha() and not i.isdigit() and i!='_' and i!='.' and i!='-':
+        s=s.replace(i,'')
+s=re.sub('\.\.*','.',s) #3
+for i in range(2):
+    s=re.sub('^\.|\.$','',s)    #4
+    if len(s)==0:   #5
+        s='a'
+    if len(s)>=16:  #6
+        s=s[:15]
+    if len(s)<=2:   #7
+        s+=s[len(s)-1]*(3-(len(s)))
+
+#Lv2
+#2 이상의 n이 입력되었을 때, n번째 피보나치 수를 1234567으로 나눈 나머지를 리턴하는 함수, solution을 완성해 주세요.
+n=int(input())
+first=0
+second=1
+hap=0
+for i in range(n):
+    if n==0 or n==1:
+        print(1)
+        break
+    else :
+        hap=first+second
+        second=first
+        first=hap
+if n!=0 and n!=1:
+    print(hap%1234567)
+
+#str에 나타나는 숫자 중 최소값과 최대값을 찾아 이를 "(최소값) (최대값)"형태의 문자열을 반환하는 함수, solution을 완성하세요.
+#예를들어 s가 "1 2 3 4"라면 "1 4"를 리턴하고, "-1 -2 -3 -4"라면 "-4 -1"을 리턴하면 됩니다.
+def solution(s):
+    s=s.replace('"','')
+    arr=list(map(int,s.split()))
+    answer = str(min(arr))+' '+str(max(arr))
+    return answer
