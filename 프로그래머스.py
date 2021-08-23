ebb500b395a2141644c4cb51e8c7c98d2e11ccd4
SH <tmdgns12j@naver.com>
2021-08-23 19:36:34 +0900

    .

--- 프로그래머스.py
+++ 프로그래머스.py
@@ -67,7 +67,11 @@ for i in range(2):
     if len(s)<=2:   #7
         s+=s[len(s)-1]*(3-(len(s)))
 
-#Lv2
+
+
+
+#-----------------------------------------------------Lv2
+
 #2 이상의 n이 입력되었을 때, n번째 피보나치 수를 1234567으로 나눈 나머지를 리턴하는 함수, solution을 완성해 주세요.
 n=int(input())
 first=0
@@ -91,3 +95,38 @@ def solution(s):
     arr=list(map(int,s.split()))
     answer = str(min(arr))+' '+str(max(arr))
     return answer
+
+
+
+
+#-----------------------------------------------------LV3
+
+# I 숫자 -> 큐에 숫자를 삽입함
+# D 1 -> 큐에서 최대값 삭제
+# D -1 -> 큐에서 최소값 삭제
+# 큐가 비어있으면 [0,0], 비어있지않으면[최대값,최소값] 출력
+#원소는 “명령어 데이터” 형식으로 주어집니다. 최댓값/최솟값을 삭제하는 연산에서 최댓값/최솟값이 둘 이상인 경우, 하나만 삭제합니다.
+#빈 큐에 데이터를 삭제하라는 연산이 주어질 경우, 해당 연산은 무시합니다.
+operations=["I 7","D -1","D 1","I 5"]
+queue=[]
+answer=[0,0]
+for i in range(len(operations)):
+    c,n=operations[i].split()
+    if c=='I':
+        n=int(n)
+        queue.append(n)
+    elif c=='D':
+        if len(queue)==0:
+            continue
+        n=int(n)
+        if n==1:
+            queue.remove(max(queue))
+        if n==-1:
+            queue.remove(min(queue))
+
+if len(queue)==0:
+    print(answer)
+else:
+    answer[0]=max(queue)
+    answer[1]=min(queue)
+    print(answer)
