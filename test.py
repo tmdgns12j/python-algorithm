bb634a4745d158552f59ceeb447b454e23f1c65d
SH <tmdgns12j@naver.com>
2021-09-07 19:13:44 +0900

    .

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-progresses=[95, 90, 99, 99, 80, 99]
-speeds=[1, 1, 1, 1, 1, 1]
-def solution(progresses, speeds):
-    answer = []
-    
+arr1=[[1, 4], [3, 2], [4, 1]]
+arr2=[[3, 3], [3, 3]]
+x,y=arr1[0]
+print(x,y)
+print(len(arr2[0]))
+def solution(arr1, arr2):
+    answer = [[]]
+    for i in range(len(arr1)): #3
+        x,y=arr1[i]# 1 4
+        for j in range(len(arr2[0])):#열
+            answer[i].append(x*arr2[0][i]+y*arr2[1][i])
     return answer
+print(solution(arr1,arr2))
