b8139404c840c819397460a9f9ca5eb8c3af4877
SH <tmdgns12j@naver.com>
2021-10-01 19:15:47 +0900

    .

--- 프로그래머스LV2.py
+++ 프로그래머스LV2.py
@@ -425,7 +425,7 @@ def solution(priorities, location):
         else:
             queue.append(temp1)
             arr.append(temp2)
-print(solution(priorities, location)
+print(solution(priorities, location))
 #다른풀이 any()
 # def solution(priorities, location):
 #     queue =  [(i,p) for i,p in enumerate(priorities)]
@@ -438,6 +438,37 @@ print(solution(priorities, location)
 #             answer += 1
 #             if cur[0] == location:
 #                 return answer
+
+
+#소수찾기 set() permutation() 조합 경우의수
+#에라토스테네스의 체 참고
+#한자리 숫자가 적힌 종이조각들이 들어있다
+#종이를 붙여 소수를 몇개 만들수있는지 출력
+from itertools import permutations
+numbers="17"
+def solution(numbers):
+    new=set()
+    answer=[]
+    numbers=list(numbers)
+    for i in numbers:
+        new.add(int(i))
+    for i in range(2,len(numbers)+1):
+        per=list(permutations(numbers,i))
+        for i in per:
+            i=int("".join(i))
+            new.add(i)
+    if 0 in new:
+        new.remove(0)
+    if 1 in new:
+        new.remove(1)
+    answer=len(new)
+    for i in new:
+        for j in range(2,i//2+1):
+            if i%j==0:
+                answer-=1
+                break
+    return answer
+print(solution(numbers))
 #-----------------------------------------------------LV3
 
 # I 숫자 -> 큐에 숫자를 삽입함
