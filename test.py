8c3d06e7a75e82524d5f4d344a7fce06b52fa5f1
SH <tmdgns12j@naver.com>
2021-10-11 17:40:35 +0900

    .

--- test.py
+++ test.py
@@ -1,25 +1,6 @@
-from itertools import permutations
-from collections import deque
-expression="100-200*300-500+20"
-cal=[]
-num=expression
-for i in range(len(expression)):
-    if expression[i].isdigit():
-        continue
-    else:
-        cal.append(expression[i])
-        num=num.replace(expression[i],' ')
-num=num.split()
-print(num)
-print(cal)
-new=['-','+','*']
-new=list(permutations(new,3))
-print(new)
-queue=deque(cal)
-# for i in range(len(new)):
-#     for j in range(len(new[0])):
-#         for k in range(len(cal)):
-for i in new:
-    a,b,c=i
-    for j in range(len(cal)):
-        if cal[j]==a:
+board=[[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]
+def solution(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j]==1:
+print(solution(board))
