bf499a10d1644672babc06af8746b309e591a2dd
SH <tmdgns12j@naver.com>
2021-10-12 18:32:18 +0900

    .

--- test.py
+++
@@ -1,6 +0,0 @@
-board=[[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]
-def solution(board):
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if board[i][j]==1:
-print(solution(board))
