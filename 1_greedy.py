90994257df0a9e1cb0fc2da26d33b56fb27bf168
SH <tmdgns12j@naver.com>
2021-10-22 18:10:22 +0900

    .

--- 1_greedy.py
+++ 1_greedy.py
@@ -194,3 +194,34 @@ for time in arr:
         start=time[1]
         count+=1
 print(count)
+
+
+#백준 1541 잃어버린 괄호
+#식이 주어진다 숫자는 0으로 시작할수있음
+#식에 괄호를 적절히 쳐서 최소값을 만들자
+#index에서 가장 멍청한 실수함
+s=input()
+cal=[]
+for i in range(len(s)):
+    if s[i].isdigit():
+        continue
+    else:
+        cal.append(s[i])
+        s=s.replace(s[i],' ',1)
+s=s.split()
+for i in range(len(s)):
+    s[i]=int(s[i])
+i=0
+while '+' in cal:
+    if cal[i]=='+':
+        s[i]+=s[i+1]
+        s[i+1]=-1 ###########추가해서 통과
+        s.pop(s.index(s[i+1]))########## 실수부분
+        cal.pop(cal.index('+'))
+        i=0
+    else:
+        i+=1
+result=s[0]
+for i in range(1,len(s)):
+    result-=s[i]
+print(result)
--- test.py
+++ test.py
@@ -1,25 +1,5 @@
-#백준 1541
-s=input()
-cal=[]
-for i in range(len(s)):
-    if s[i].isdigit():
-        continue
-    else:
-        cal.append(s[i])
-        s=s.replace(s[i],' ',1)
-s=s.split()
-for i in range(len(s)):
-    s[i]=int(s[i])
-i=0
-while '+' in cal:
-    if cal[i]=='+':
-        s[i]+=s[i+1]
-        s.pop(s.index(s[i+1]))
-        cal.pop(cal.index('+'))
-        i=0
-    else:
-        i+=1
-result=s[0]
-for i in range(1,len(s)):
-    result-=s[i]
-print(result)
+#13305 주유소
+n=4
+length=list(map(int,input().split()))
+price=list(map(int,input().split()))
+print(price)
