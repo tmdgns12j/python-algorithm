1c2b387ca198988bb7e7cdffe6faa41331d6d7aa
SH <tmdgns12j@naver.com>
2021-11-17 17:13:21 +0900

    .

--- unsolved.py
+++ unsolved.py
@@ -227,3 +227,26 @@ def solution(name):
             answer+=1
     return answer
 print(solution(name))
+
+# #백준
+# from collections import deque
+# n=int(input())
+# for i in range(n):
+#     command=input()
+#     command=command.replace('RR','')
+#     c=int(input())
+#     arr=input()[1:-1].split(',')
+#     queue=deque(arr)
+#     if c==0:
+#         arr=[]
+#     for j in command:
+#         if j=='R':
+#             arr=arr[::-1]
+#         else:
+#             if len(arr)==0:
+#                 print("error")
+#                 break
+#             else:
+#                 arr.pop(0)
+#     else:
+#         print('['+','.join(arr)+']')
--- 문자열.py
+++ 문자열.py
@@ -14,3 +14,29 @@ for i in range(len(test)):
 for i in alpha:
     answer+=str(count[i])+' '
 print(answer)
+
+
+#백준 1157
+#알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳출력
+#한개가아닐시 ?출력
+#set()중복제거, 중복불가자료형
+#Counter
+from typing import Counter
+# s='Mississsiiiisipi'
+s=input().lower()
+alpha=list(set(s))
+a=Counter(s)
+
+max=0
+for i in alpha:
+    if a[i]>max:
+        max=a[i]
+c=0
+for i in alpha:
+    if a[i]==max:
+        temp=i
+        c+=1
+if c==1:
+    print(temp.upper())
+else:
+    print("?")
