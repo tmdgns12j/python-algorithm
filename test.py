215f82359024b1a440907103c8c86173776169d6
SH <tmdgns12j@naver.com>
2021-11-25 00:45:23 +0900

    .

--- test.py
+++ test.py
@@ -1,15 +1,26 @@
-s='happy'
-temp=''
-alpha='abcdefghijklmnopqrstuvwxyz'
-visited=dict()
-c=0
-for i in alpha:
-    visited[i]=-1
-print(visited)
-for i in range(len(s)):
-    if temp==s[i]:
-        continue
-    temp=s[i]
-    if visited[s[i]]!=-1:
-        break
-    
+import sys
+n=int(sys.stdin.readline())
+arr=[]
+for i in range(n):
+    temp=0
+    command=sys.stdin.readline().split()
+    if len(command)==2:
+        temp=int(command[1])
+        arr.append(temp)
+    elif command[0]=="top":
+        if len(arr)==0:
+            print('-1')
+        else:
+            print(arr[-1])
+    elif command[0]=='size':
+        print(len(arr))
+    elif command[0]=='empty':
+        if len(arr)==0:
+            print('1')
+        else:
+            print('0')
+    elif command[0]=='pop':
+        if len(arr)==0:
+            print('-1')
+        else:
+            print(arr.pop())
--- 문자열.py
+++ 문자열.py
@@ -65,3 +65,20 @@ for j in range(c):
             else:
                 print("NO")
                 break
+
+
+
+s='happy'
+temp=''
+alpha='abcdefghijklmnopqrstuvwxyz'
+visited=dict()
+c=0
+for i in alpha:
+    visited[i]=-1
+print(visited) 
+for i in range(len(s)):
+    if temp==s[i]:
+        continue
+    temp=s[i]
+    if visited[s[i]]!=-1:
+        break
